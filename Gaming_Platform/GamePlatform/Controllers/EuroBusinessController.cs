fc5893c41033f48105760b5296c10bf455fc3ac6
KamilStepien <stepienkamil97@gmail.com>
2020-01-22 19:36:02 +0100

    Test communication between the view and the controller

--- Gaming_Platform/GamePlatform/Controllers/EuroBusinessController.cs
+++ Gaming_Platform/GamePlatform/Controllers/EuroBusinessController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using GamePlatform.Models.Monopoly;
 
 namespace GamePlatform.Controllers
 {
     public class EuroBusinessController : Controller
     {
+        Dice dice = new Dice(1, 6);
+       
+
         public IActionResult EuroBusiness()
         {
+            
             return View();
+        }
+
+        public IActionResult roll()
+        {
+
+            TempData["DiceValue"] =  dice.Rol();
+
+            return RedirectToAction("EuroBusiness");
         }
     }
 }
--- Gaming_Platform/GamePlatform/Models/Monopoly/Dice.cs
+++ Gaming_Platform/GamePlatform/Models/Monopoly/Dice.cs
@@ -7,5 +7,18 @@ namespace GamePlatform.Models.Monopoly
 {
     public class Dice
     {
+        Random rnd = new Random();
+        int _max;
+        int _min;
+
+        public Dice(int min = 1 , int max =6)
+        {
+            _min = min;
+            _max = max;
+        }
+
+
+        public  int  Rol() => rnd.Next(_min, _max);
+
     }
 }
