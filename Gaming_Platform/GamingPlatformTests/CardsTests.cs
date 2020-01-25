47808a263015ddb861131a9684d5791f7897006d
Dominik Toczek <dominiktoczek@interia.pl>
2020-01-25 11:20:43 +0100

    Add tests for DeckOfCards

---
+++ Gaming_Platform/GamingPlatformTests/CardsTests.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using NUnit.Framework;
+using Cards;
+using System.Linq;
+using System;
+
+namespace GamingPlatformTests
+{
+    [TestFixture]
+    class CardsTests
+    {
+        [Test]
+        public void CreateDeck_WhenCalled_DeckOfCardsEquals52Cards()
+        {
+            DeckOfCards _deck = new DeckOfCards();
+            _deck.CreateDeck();
+            var expected = 52;
+            var actual = _deck.Deck.Length;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ShuffleDeck_WhenCalled_CardsInDeckWereShuffled()
+        {
+            DeckOfCards _deck = new DeckOfCards();
+            _deck.CreateDeck();
+            var expected = _deck.Deck;
+            _deck.ShuffleDeck();
+            var actual = _deck.Deck;
+            for (int i=0; i<expected.Length-1; i++)
+            {
+                Assert.AreNotSame(
+                    new Tuple<Card.SUIT, Card.VALUE>(expected[i].CardSuit, expected[i].CardValue),
+                    new Tuple<Card.SUIT, Card.VALUE>(actual[i].CardSuit, actual[i].CardValue)
+                    );
+            }
+        }
+    }
+}
