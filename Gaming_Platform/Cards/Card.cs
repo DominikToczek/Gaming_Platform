6768436e787599f3af9ce3daea8a8be7df3e30f9
Dominik Toczek <dominiktoczek@interia.pl>
2020-01-25 15:58:28 +0100

    Add constructor test for Card.cs

--- Gaming_Platform/Cards/Card.cs
+++ Gaming_Platform/Cards/Card.cs
@@ -5,6 +5,14 @@
         public SUIT CardSuit { get; set; }
         public VALUE CardValue { get; set; }
 
+        public Card(SUIT suit, VALUE value)
+        {
+            CardSuit = suit;
+            CardValue = value;
+        }
+
+        public Card() { }
+
         public enum SUIT
         {
             HEARTS,
