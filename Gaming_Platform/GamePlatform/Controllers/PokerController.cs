cea8a8c226b16f7ee9c95ec0d6276a8dd8a191d5
ViperEvo <speed6767@wp.pl>
2020-01-26 16:49:40 +0100

    Poker

---
+++ Gaming_Platform/GamePlatform/Controllers/PokerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GamePlatform.Models;
+
+namespace GamePlatform.Controllers
+{
+    public class PokerController : Controller
+    {
+        List<IPlayer> players;
+
+
+        public PokerController()
+        {
+
+        }
+         List<IPlayer> initializationPlayer()
+        {
+            List<IPlayer> players =new List<IPlayer>(); ;
+            players.Add(new Player("Arek", 0));
+            return players;
+        }
+
+        public ViewResult Poker()
+        {
+            players = initializationPlayer();
+            ViewData["Players"] = players;
+            return View();
+        }
+
+        public IActionResult click(int button)
+        {
+
+            players[button].AddMoney(1000);
+            ViewData["Players"] = players;
+            return PartialView("Poker");
+        }
+
+       
+    }
+
+
+
+    }
---
+++ Gaming_Platform/GamePlatform/wwwroot/js/Poker.js
@@ -0,0 +1,27 @@
+﻿function newGame() {
+    console.log("Nowa gra");
+}
+
+function checkGame() {
+    console.log("Sprawdz");
+}
+
+function changeCard1() {
+    console.log("Karta 1");
+}
+
+function changeCard2() {
+    console.log("Karta 2");
+}
+
+function changeCard3() {
+    console.log("Karta 3");
+}
+
+function changeCard4() {
+    console.log("Karta 4");
+}
+
+function changeCard5() {
+    console.log("Karta 5");
+}
