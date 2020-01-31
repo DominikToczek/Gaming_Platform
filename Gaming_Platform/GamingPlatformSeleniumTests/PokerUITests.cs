b76f7251ef97d575a4cdeaf46f0bd53788cd4a61
Dominik Toczek <dominiktoczek@interia.pl>
2020-01-31 14:58:06 +0100

    Add new project - selenium tests

---
+++ Gaming_Platform/GamingPlatformSeleniumTests/PokerUITests.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace GamingPlatformSeleniumTests
+{
+    public class PokerUITests : IDisposable
+    {
+        private readonly IWebDriver _driver;
+
+        public PokerUITests()
+        {
+            _driver = new ChromeDriver();
+
+        }
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
