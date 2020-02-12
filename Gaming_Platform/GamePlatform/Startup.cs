a62380bff1786bcfcd82ce69d59938d5aab191b7
Dominik Toczek <dominiktoczek@interia.pl>
2020-02-12 19:00:59 +0100

    Added service-singleton to controller

--- Gaming_Platform/GamePlatform/Startup.cs
+++ Gaming_Platform/GamePlatform/Startup.cs
@@ -24,6 +24,7 @@ namespace GamePlatform
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddSingleton<Game>();
+            services.AddSingleton<Deal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
