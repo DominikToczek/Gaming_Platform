802da3b7d866102c615d14d9430f40e698430447
KamilStepien <stepienkamil97@gmail.com>
2020-02-13 21:00:08 +0100

    Add enum FieldType

---
+++ Gaming_Platform/GamePlatform/Models/Monopoly/FieldTypecs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamePlatform.Models
+{
+    public enum FieldType
+    {
+        Start,
+        City,
+        Empty,
+    
+    }
+}
