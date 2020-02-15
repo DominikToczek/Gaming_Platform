3cfaa0b749d84008e14ab2ae09747a21ab5ad70b
Toczek Dominik <dominik.toczek@microsoft.wsei.edu.pl>
2020-02-15 12:43:37 +0100

    changed GUI

--- Gaming_Platform/GamePlatform/wwwroot/js/Poker.js
+++ Gaming_Platform/GamePlatform/wwwroot/js/Poker.js
@@ -1,7 +1,14 @@
-﻿function newGame() {
+﻿let cardClicked1 = false;
+let cardClicked2 = false;
+let cardClicked3 = false;
+let cardClicked4 = false;
+let cardClicked5 = false;
+
+function newGame() {
     dealCards();
     getPlayerHand();
     getComputerHand();
+    resetChangeCardsButtons();
 }
 
 function checkGame() {
@@ -17,24 +24,47 @@ function checkGame() {
     })
 }
 
+function resetChangeCardsButtons() {
+    cardClicked1 = false;
+    cardClicked2 = false;
+    cardClicked3 = false;
+    cardClicked4 = false;
+    cardClicked5 = false;
+}
+
 function changeCard1() {
-    changeCard(1);
+    if (!cardClicked1) {
+        changeCard(1);
+        cardClicked1 = true;
+    }
 }
 
 function changeCard2() {
-    changeCard(2);
+    if (!cardClicked2) {
+        changeCard(2);
+        cardClicked2 = true;
+    }
 }
 
 function changeCard3() {
-    changeCard(3);
+    if (!cardClicked3) {
+        changeCard(3);
+        cardClicked3 = true;
+    }
 }
 
 function changeCard4() {
-    changeCard(4);
+    if (!cardClicked4) {
+        changeCard(4);
+        cardClicked4 = true;
+    }
 }
 
 function changeCard5() {
-    changeCard(5);
+    if (!cardClicked5) {
+        changeCard(5);
+        cardClicked5 = true;
+    }
 }
 
 function dealCards() {
@@ -68,6 +98,7 @@ function getComputerHand() {
         type: "POST",
         url: "/Poker/GetComputerHand",
         success: function (response) {
+            displayCards(response)
             console.log(response)
         },
         error: function (response) {
@@ -88,4 +119,17 @@ function changeCard(Id) {
             console.log("Coś poszło nie tak" + response)
         }
     })
+}
+
+function displayCards(response) {
+    document.getElementById("card-suit-1").innerHTML = response.card1Suit;
+    document.getElementById("card-value-1").innerHTML = response.card1Value;
+    document.getElementById("card-suit-2").innerHTML = response.card2Suit;
+    document.getElementById("card-value-2").innerHTML = response.card2Value;
+    document.getElementById("card-suit-3").innerHTML = response.card3Suit;
+    document.getElementById("card-value-3").innerHTML = response.card3Value;
+    document.getElementById("card-suit-4").innerHTML = response.card4Suit;
+    document.getElementById("card-value-4").innerHTML = response.card4Value;
+    document.getElementById("card-suit-5").innerHTML = response.card5Suit;
+    document.getElementById("card-value-5").innerHTML = response.card5Value;
 }
